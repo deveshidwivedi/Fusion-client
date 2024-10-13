581d7402e8cb65ee7b199bc602e715700e318fb3
abansal15 <2022abansal@gmail.com>
2024-10-13 14:04:55 +0530

    corrected the route of reports page from /Reports to /inventory/reports

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ export default function App() {
         />
 
         <Route
-          path="/inventory/reports"
+          path="/inventory/report"
           element={
             <Layout>
               <Reports />
